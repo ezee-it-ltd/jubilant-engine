f768b281c83ebae80e506b400b6dae339275c16e
grandmaskitchen <garypickett62@gmail.com>
2026-02-09 18:35:11 +0000

    Fix main.tsx: move dev badge into React tree and set router future flags

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,36 @@ import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./index.css";
 
+function DevBadge() {
+  if (!import.meta.env.DEV) return null;
+  return (
+    <div
+      style={{
+        position: "fixed",
+        bottom: 8,
+        right: 8,
+        fontSize: 11,
+        opacity: 0.5,
+        pointerEvents: "none",
+        zIndex: 9999,
+      }}
+    >
+      {window.location.origin}
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <App />
+        <DevBadge />
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
