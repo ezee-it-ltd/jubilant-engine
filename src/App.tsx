c9db12b918e22770cff6b68eff63747a1b746f5f
grandmaskitchen <garypickett62@gmail.com>
2026-02-11 19:27:14 +0000

    Fix: use relative page imports for Cloudflare build

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "@/pages/Home";
-import Notebook from "@/pages/Notebook";
-// import other pages...
+
+import Home from "./pages/Home";
+import Notebook from "./pages/Notebook";
+import ShoppingList from "./pages/ShoppingList";
+import PrintNotebook from "./pages/PrintNotebook";
+import PrintShoppingList from "./pages/PrintShoppingList";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/notebook" element={<Notebook />} />
-      {/* other routes... */}
+      <Route path="/shopping-list" element={<ShoppingList />} />
+      <Route path="/print-shopping-list" element={<PrintShoppingList />} />
+      <Route path="/print/:scope?" element={<PrintNotebook />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
